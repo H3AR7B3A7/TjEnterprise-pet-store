1b3088deeba579adbc1336e42d0d290c98142330
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2019-12-16 21:47:35 +0100

    Added Javadoc

--- src/main/java/es/egven/tjenterprise/controllers/CartController.java
+++ src/main/java/es/egven/tjenterprise/controllers/CartController.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * CART CONTROLLER
+ * @author d.Dog
+ */
 @Controller
 public class CartController {
     @GetMapping(value = {"/cart"})
