b24c9aaf156a908316146011069b0fdf7b607c9e
etixz <egon.bomon2801@hotmail.com>
2019-12-16 15:18:37 +0100

    Added @Autowired to every controller Added ShowIndex + modelattribute to IndexController

--- src/main/java/es/egven/tjenterprise/controllers/CartController.java
+++ src/main/java/es/egven/tjenterprise/controllers/CartController.java
@@ -1,8 +1,12 @@
 package es.egven.tjenterprise.controllers;
 
+import es.egven.tjenterprise.model.ProductDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class CartController {
 
+    @Autowired
+    ProductDAO dao;
 }
