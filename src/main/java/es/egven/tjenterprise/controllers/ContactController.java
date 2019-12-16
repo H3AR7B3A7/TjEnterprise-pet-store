32109ef3b07c3fdb6b8ec360fbc22d66eb1b1d72
etixz <egon.bomon2801@hotmail.com>
2019-12-16 15:22:11 +0100

    Added showContactPage request map to ContactController

--- src/main/java/es/egven/tjenterprise/controllers/ContactController.java
+++ src/main/java/es/egven/tjenterprise/controllers/ContactController.java
@@ -3,10 +3,17 @@ package es.egven.tjenterprise.controllers;
 import es.egven.tjenterprise.model.ProductDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ContactController {
 
     @Autowired
     ProductDAO dao;
+
+    @RequestMapping(value = "/contact")
+    public String showContactPage(){
+        return "contact";
+    }
 }
