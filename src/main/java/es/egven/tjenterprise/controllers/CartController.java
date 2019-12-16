3322076e8c0a0635d1b8a4f0a24fcf3cc8b53ec2
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2019-12-16 19:29:10 +0100

    Set contact ctrl

--- src/main/java/es/egven/tjenterprise/controllers/CartController.java
+++ src/main/java/es/egven/tjenterprise/controllers/CartController.java
@@ -2,7 +2,6 @@ package es.egven.tjenterprise.controllers;
 
 import es.egven.tjenterprise.model.Cart;
 import es.egven.tjenterprise.model.CartDAO;
-import es.egven.tjenterprise.model.ProductDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- src/main/java/es/egven/tjenterprise/controllers/ContactController.java
+++ src/main/java/es/egven/tjenterprise/controllers/ContactController.java
@@ -1,18 +1,12 @@
 package es.egven.tjenterprise.controllers;
 
-import es.egven.tjenterprise.model.ProductDAO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ContactController {
 
-    @Autowired
-    ProductDAO dao;
-
-    @RequestMapping(value = "/contact")
+    @GetMapping(value = "/contact")
     public String showContactPage(){
         return "contact";
     }
