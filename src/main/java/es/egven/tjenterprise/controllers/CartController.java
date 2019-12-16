0b9c601cb8aaeeb8920ba78984c1aa9255bda1cf
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2019-12-16 16:56:34 +0100

    Added housestyle .. Just to see the effect

--- src/main/java/es/egven/tjenterprise/controllers/CartController.java
+++ src/main/java/es/egven/tjenterprise/controllers/CartController.java
@@ -1,9 +1,14 @@
 package es.egven.tjenterprise.controllers;
 
+import es.egven.tjenterprise.model.Cart;
 import es.egven.tjenterprise.model.CartDAO;
 import es.egven.tjenterprise.model.ProductDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * PRODUCT CONTROLLER
@@ -13,6 +18,40 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class CartController {
 
+    /**
+     * Made dao private
+     */
     @Autowired
-    CartDAO dao;
+    private CartDAO dao;
+
+    /**
+     * Call dao to get public data
+     * @return Dao for Cart
+     */
+    public CartDAO getDao(){
+        return dao;
+    }
+
+    /**
+     * Add attribute cartId to map
+     * @param model
+     * @return cart
+     */
+    @RequestMapping("cart/getCartById")
+    public String getCartId(Model model){
+        model.addAttribute("cartId");
+        return "cart";
+    }
+
+    /**
+     * Fetch CartItems
+     * @param cartId
+     * @return Gets CartItems in cart by cartId
+     */
+    @RequestMapping("cart/{cartId}")
+    public @ResponseBody
+    Cart getCartItems(@PathVariable(value = "cartId")String cartId){
+        return getCartItems(cartId);
+    }
+
 }
