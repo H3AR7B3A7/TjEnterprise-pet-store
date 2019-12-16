a010de9b93a248f21e84b061b4d703a82bd66e9a
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2019-12-16 21:15:54 +0100

    Setting up order logic

--- src/main/java/es/egven/tjenterprise/controllers/OrderController.java
+++ src/main/java/es/egven/tjenterprise/controllers/OrderController.java
@@ -1,11 +1,12 @@
 package es.egven.tjenterprise.controllers;
 
+import es.egven.tjenterprise.model.Cart;
 import es.egven.tjenterprise.model.Order;
 import es.egven.tjenterprise.model.OrderDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+
 
 @Controller
 public class OrderController {
@@ -13,13 +14,12 @@ public class OrderController {
     @Autowired
     private OrderDAO dao;
 
-    @GetMapping("/order/{cartId}")
-    public String createOrder(@PathVariable("cartId")int cartId){
+    @GetMapping("/cart/order")
+    public String createOrder(){
         Order order = new Order();
         // TODO : add logic to add cart to order and save
-
-        return "redirect:checkout?cartId="+ cartId;
+            order.getOrderContent(Cart.toString());
+        return "redirect:checkout";
     }
-
 
 }
