69b0796c572bee9e436ace84c3cb6f49711f34c2
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2019-12-17 21:50:18 +0100

    Page styling

--- src/main/java/es/egven/tjenterprise/controllers/CartController.java
+++ src/main/java/es/egven/tjenterprise/controllers/CartController.java
@@ -4,17 +4,9 @@ import es.egven.tjenterprise.model.Cart;
 import es.egven.tjenterprise.model.Product;
 import es.egven.tjenterprise.model.ProductDAO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.HashMap;
 
 /**
  * CART CONTROLLER
@@ -26,51 +18,33 @@ public class CartController {
     @Autowired
     ProductDAO dao;
 
+    /**
+     * Model attribute for Cart
+     * @return Cart()
+     */
     @ModelAttribute("nCart")
-    public Cart winkelKarAanmaken() {
+    public Cart createNewCart() {
         return new Cart();
     }
-/*
-    @GetMapping(value = "/cart")
-    public String showCart(ModelMap map){
-        return "cart";
-    }*/
 
+    /**
+     * Model attribute for Product
+     * @return Product()
+     */
     @ModelAttribute("nProduct")
     public Product productToAdd(){
         return new Product();
     }
 
-   /*@RequestMapping(value = "/cart", method = RequestMethod.POST)
-    public String addProduct(@ModelAttribute("nProduct") @Valid Product nProduct, BindingResult bindingResult
-                             /*@ModelAttribute("nCart") Cart nCart){
-        if(bindingResult.hasErrors()){
-            return "contact";
-        }
-        Cart.addItemToCart(nProduct);
-        return "cart";
-    }*/
-
-    /*@RequestMapping(method = RequestMethod.GET, value="/cart")
-    @ResponseBody
-    public ArrayList<Product> toonWinkelKar(){return Cart.cartContent;}*/
-
+    /**
+     * Add a product to cart
+     * @param id
+     * @return Home page, to keep adding to cart
+     */
     @GetMapping(value = "/{id}")
     public String addProductToCart(@PathVariable(value = "id") int id){
         Product nProduct = dao.findById(id).get();
         Cart.addItemToCart(nProduct);
-
         return "redirect:/index";
-
     }
-
-
-
-    /*@GetMapping
-    public Cart getCart(){
-        nCart.getCartContent();
-        nCart.getTotalPrice();
-    }*/
-
-
 }
