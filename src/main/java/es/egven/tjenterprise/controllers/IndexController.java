b2221ac2a25912fe4c1d9751bc64c946593fbb1f
etixz <egon.bomon2801@hotmail.com>
2019-12-17 16:44:44 +0100

    IndexController updated (working on filters)

--- src/main/java/es/egven/tjenterprise/controllers/IndexController.java
+++ src/main/java/es/egven/tjenterprise/controllers/IndexController.java
@@ -4,10 +4,13 @@ import es.egven.tjenterprise.model.Product;
 import es.egven.tjenterprise.model.ProductDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
@@ -24,4 +27,23 @@ public class IndexController {
     public String showIndex(ModelMap map){
         return "index";
     }
+
+    @ModelAttribute("productFilerDier")
+    public Iterable<Product> showProductCategorieDier(){
+        return dao.findByCategorieDier("Dier");
+    }
+    @ModelAttribute("productFilterToy")
+    public Iterable<Product> showProductCategorieToy(){
+        return dao.findByCategorieDier("Toy");
+    }
+    @ModelAttribute("productFilterFood")
+    public Iterable<Product> showProductCategorieFood(){
+        return dao.findByCategorieDier("Food");
+    }
+    @ModelAttribute("productFilterHabitat")
+    public Iterable<Product> showProductCategorieHabitat(){
+        return dao.findByCategorieDier("Habitat");
+    }
+
 }
+
--- src/main/java/es/egven/tjenterprise/model/ProductDAO.java
+++ src/main/java/es/egven/tjenterprise/model/ProductDAO.java
@@ -1,6 +1,11 @@
 package es.egven.tjenterprise.model;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProductDAO extends CrudRepository<Product, Integer> {
+
+    @Query(value = "select p from Product p where p.productCategorie = :productCategorie")
+    Iterable<Product> findByCategorieDier(String productCategorie);
+
 }
