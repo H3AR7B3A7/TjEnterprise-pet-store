bcc93c99fb0e63d3889bf8702da3420348dbf19b
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2019-12-18 01:25:44 +0100

    Added error handling and page

--- src/main/java/es/egven/tjenterprise/controllers/CartController.java
+++ src/main/java/es/egven/tjenterprise/controllers/CartController.java
@@ -6,7 +6,6 @@ import es.egven.tjenterprise.model.Product;
 import es.egven.tjenterprise.model.ProductDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
